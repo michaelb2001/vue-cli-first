8f1494db99b8847a963d44a3064a43a65c2b177d
michaelb2001 <brunco.michael@gmail.com>
2022-01-26 14:10:49 +0100

    creazione Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="jumbotron">
+
+    <div class="top">
+      <h3> questo è il logo di Vue.js <br>
+        cliccaci sopra per scoprire la guida &rarr; </h3>
+      <a href="https://vuejs.org/">
+        <img src="./assets/logo.png" alt="logo">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+*{
+  padding: 0%;
+  margin: 0%;
+  box-sizing: border-box;
+}
+
+.jumbotron{
+  min-height: 120px;
+  width: 70%;
+  background-color: rgb(152, 152, 152) ;
+  border-radius: 25px;
+  padding: 20px;
+}
+
+h3 {
+  text-align: center;
+  color: blue;
+  text-transform: capitalize;
+}
+
+a img{
+  width: 30px;
+  height: auto;
+}
+
+
+</style>
