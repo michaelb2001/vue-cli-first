6781fb3be0d64a44da7cd729d610b55e6348f393
michaelb2001 <brunco.michael@gmail.com>
2022-01-26 13:53:36 +0100

    creazione componente Title

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>La mia prima app con Vue CLI!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  text-align: center;
+  text-decoration: underline;
+  color: green;
+  text-transform: capitalize;
+}
+</style>
